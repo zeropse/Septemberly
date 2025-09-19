f10b0a0137dcb466669efb9944a120454bc8a12c
zeropse <129523087+zeropse@users.noreply.github.com>
2025-09-20 03:55:01 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/8bit/button";
+
+const App = () => {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Button>Click ME</Button>
+    </>
+  );
+};
+
+export default App;
